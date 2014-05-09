5c134c0cb2674b600ce00860677ea0dba69a2bb1
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-05-09 13:09:41 -0500

    Do not output results into files

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 NAME="testproject"
 GIT_URL=$1
 GIT_SHA=$2
@@ -26,10 +24,10 @@ docker run --name=$NAME codecheck git clone $GIT_URL $PROJECT_PATH
 IMAGE_ID=$(docker commit $NAME)
 
 # Execute metrics
-docker run --rm=true $IMAGE_ID sandi_meter -d -p --json $PROJECT_PATH > sandi_meter.json
-docker run --rm=true $IMAGE_ID brakeman --format json $PROJECT_PATH > brakeman.json
-docker run --rm=true $IMAGE_ID flog --json --19 -q -g $PROJECT_PATH > flog.json
-docker run --rm=true $IMAGE_ID rails_best_practices --format json $PROJECT_PATH > rails_best_practices.json
+docker run --rm=true $IMAGE_ID sandi_meter -d --json -p $PROJECT_PATH
+docker run --rm=true $IMAGE_ID brakeman --format json $PROJECT_PATH
+docker run --rm=true $IMAGE_ID flog --json --19 -q -g $PROJECT_PATH
+docker run --rm=true $IMAGE_ID rails_best_practices --format json $PROJECT_PATH
 
 # Remove image
 docker rmi $IMAGE_ID
